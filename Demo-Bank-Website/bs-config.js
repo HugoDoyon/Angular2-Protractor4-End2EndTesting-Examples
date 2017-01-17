6c0e49673c0c1a4ed0710c4544b2000ba84c3af1
Hugo Doyon <hugo.doyon@gmail.com>
2017-01-16 21:39:05 -0500

    disable sync for lite-server

---
+++ Demo-Bank-Website/bs-config.js
@@ -0,0 +1,25 @@
+'use strict';
+var fallback = require('connect-history-api-fallback');
+var log = require('connect-logger');
+/*
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ */
+module.exports = {
+  injectChanges: false, // workaround for Angular 2 styleUrls loading
+  files: ['./**/*.{html,htm,css,js}'],
+  watchOptions: {
+    ignored: 'node_modules'
+  },
+ ghostMode: false,
+  server: {
+    baseDir: './',
+    middleware: [
+      log({ format: '%status %method %url' }),
+      fallback({
+        index: '/index.html',
+        htmlAcceptHeaders: ['text/html', 'application/xhtml+xml'] // systemjs workaround
+      })
+    ]
+  }
+};
